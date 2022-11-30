8db965ae9371adc0d659f3c0814404cf5838a46f
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-11-30 10:02:01 +0530

    changing styles from JS

--- HTML/javascript.js
+++ HTML/javascript.js
@@ -2,5 +2,12 @@
 
 function changeName() {
   document.getElementById("demo").innerHTML = "Monika Abhijeet";
-  window.print();
+}
+
+function changeColor(color) {
+  document.getElementById("demo").style.color = color;
+}
+
+function hideText() {
+  document.getElementById("hide").style.display = "none";
 }
