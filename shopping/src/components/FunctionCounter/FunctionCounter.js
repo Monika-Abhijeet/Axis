9d4397f1508d62c9cd18bf05cca460b59636b380
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-12-06 14:36:19 +0530

    user input

--- shopping/src/components/FunctionCounter/FunctionCounter.js
+++ shopping/src/components/FunctionCounter/FunctionCounter.js
@@ -7,18 +7,36 @@ function FunctionCounter(props) {
   // }
 
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
   function incrementCount() {
     setCount(count + 1);
   }
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
 
+  function changeName(event) {
+    setName(event.target.value);
+  }
   return (
     <div className="function-counter-wrapper">
       <h1> {props.name}</h1>
       <h3>{props.subtitle}</h3>
       {/* <h3>{this.state.count} */}
       <h3>{count}</h3>
-
       <button onClick={() => incrementCount()}>Increment</button>
+      {/* <button onClick={() => decrementCount()}>Decrement</button> */}
+      <button onClick={decrementCount}>Decrement</button>
+
+      <input
+        type="text"
+        value={name}
+        onChange={(event) => {
+          changeName(event);
+        }}
+      />
+      <p>Name is {name}</p>
     </div>
   );
 }
