21beabe3d0140b60046d250774c4168f474ed7ed
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-12-06 14:15:12 +0530

    increment count

--- shopping/src/components/FunctionCounter/FunctionCounter.js
+++ shopping/src/components/FunctionCounter/FunctionCounter.js
@@ -1,16 +1,24 @@
 import { useState } from "react";
 import "./FunctionCounter.css";
+
 function FunctionCounter(props) {
   // this.state = {
   // count:0
   // }
-  const [count, setCount] = useState(100);
+
+  const [count, setCount] = useState(0);
+  function incrementCount() {
+    setCount(count + 1);
+  }
+
   return (
     <div className="function-counter-wrapper">
       <h1> {props.name}</h1>
       <h3>{props.subtitle}</h3>
       {/* <h3>{this.state.count} */}
       <h3>{count}</h3>
+
+      <button onClick={() => incrementCount()}>Increment</button>
     </div>
   );
 }
