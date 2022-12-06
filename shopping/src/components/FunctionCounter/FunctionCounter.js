2bfe087ab09450386aa3c1bd049cf61a58cf3489
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-12-06 11:51:11 +0530

    state in class and functional components

--- shopping/src/components/FunctionCounter/FunctionCounter.js
+++ shopping/src/components/FunctionCounter/FunctionCounter.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 function FunctionCounter(props) {
+  const [count, setCount] = useState(100);
   return (
     <div>
       <h1> {props.name}</h1>
       <h3>{props.subtitle}</h3>
+      <h3>{count}</h3>
     </div>
   );
 }
