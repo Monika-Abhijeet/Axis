61862e1c93139b2bf4b78539ac2fb27006a5fa58
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-12-07 14:40:20 +0530

    using Links

--- shopping/src/components/RoutingDemo/Listing.js
+++ shopping/src/components/RoutingDemo/Listing.js
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Listing() {
   return (
     <div>
       <h1>Listing page</h1>
+      <ul>
+        <Link to="/home">
+          <li>Home</li>
+        </Link>
+        <Link to="/about">
+          <li>About</li>
+        </Link>
+      </ul>
     </div>
   );
 }
