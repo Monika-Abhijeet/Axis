11179aa9e652a8080032bbc0679c6e5010b17c24
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-12-07 10:14:46 +0530

    axios requests

--- shopping/src/components/UsersList/UsersList.js
+++ shopping/src/components/UsersList/UsersList.js
@@ -4,13 +4,14 @@ function UserList() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
+      console.log(response.data);
       setUsers(response.data);
     });
   }, []);
 
   function postUserData() {
     let requestBody = {
-      name: "Monika",
+      name123: "Monika",
       username: "monu",
       email: "monu@gmail.com",
     };
@@ -20,6 +21,27 @@ function UserList() {
         console.log(response);
       });
   }
+
+  function updateUser(id) {
+    let requestBody = {
+      name: "Monika",
+      username: "monu",
+    };
+    axios
+      .put(`https://jsonplaceholder.typicode.com/users/${id}`, requestBody)
+      .then((response) => {
+        console.log(response);
+      });
+  }
+
+  function deleteUser(id) {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => {
+        console.log(response);
+      });
+  }
+
   return (
     <div>
       <h1>Post request</h1>
@@ -33,8 +55,10 @@ function UserList() {
       <h1>User List</h1>
       {users.map((user) => {
         return (
-          <p>
-            {user.name} - {user.email}
+          <p key={user.id}>
+            {user.id} - {user.name} - {user.email}{" "}
+            <button onClick={() => updateUser(user.id)}>Update</button>
+            <button onClick={() => deleteUser(user.id)}>Delete</button>
           </p>
         );
       })}
